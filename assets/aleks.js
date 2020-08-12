811cb403c58239a7a79329754cb0a42bdfac1a63
Robin <jacquemart.rob@gmail.com>
2020-08-12 15:40:28 +0200

    Continuation API et Graph

--- assets/aleks.js
+++ assets/aleks.js
@@ -1,4 +1,5 @@
 //recup des donnés
+let h1 = document.getElementById("firstHeading")
 FETCH();
 
 function FETCH() {
@@ -6,23 +7,67 @@ function FETCH() {
         const findFETCH = async function () {
             let recuperer = await fetch("https://canvasjs.com/services/data/datapoints.php");
             if (recuperer.ok) {
-                let data = await recuperer.json().then(recup => {
-                    let tableau = recup.tableau;
-                    document.getElementById("h1").innerHTML = tableau
+                let data = await recuperer.json().then(data => {
+                    // let tableau = data.tableau;
+                    console.log(data)
+                    // document.getElementById("firstHeading").innerHTML = tableau
                 });
-            }
-            else {
+
+            } else {
                 console.log("error: + recuperer.status");
             }
 
         };
         findFETCH();
-        document.getElementById("h1").addEventListener(''), () => {
-            findFETCH();
 
-        };
+        // h1.addEventListener(''), () => {
+        //     findFETCH();
+
+        // };
     })();
 }
 
+// let getData = async () => {
+//     let recuperer = await fetch('https://canvasjs.com/services/data/datapoints.php')
+//     let data = await recuperer.json()
+//     return data
+// }
 
+// getData()
+//     .then(data => {
+//         console.log(data)
+//     })
+//     .catch(error => {
+//         alert('ERREUR')
+//         console.log(error)
+//     })
+
+
+let c2 = document.createElement("canvas")
+let ctx2 = c2.getContext('2d')
+c2.width = 400;
+c2.height = 200;
+
+document.body.appendChild(c2)
+h1.insertAdjacentElement("afterend", c2)
+
+let canvApi = new Chart(c2, {
+    // Types of chart we use
+    type: 'line',
+
+    // The data of the API
+    data: {
+        labels: [''],
+        datasets: [{
+            label: 'Belgium',
+            backgroundColor: 'transparent',
+            hoverBorderColor: 'rgb(200,80,162)',
+            data: [],
+
+        }]
+    },
+    options: {
+    }
+})
+console.log(crimes.data.labels)
 
--- assets/canvas.js
+++ assets/canvas.js
@@ -4,7 +4,7 @@
 let table1 = document.getElementById("table1")
 let table2 = document.getElementById("table2")
 
-// Canvas
+// Canvas Crimes
 let c = document.createElement("canvas")
 let ctx = c.getContext('2d')
 c.width = 400;
@@ -12,25 +12,58 @@ c.height = 200;
 
 document.body.appendChild(c)
 table1.insertAdjacentElement("beforebegin", c)
+let tr = table1.getElementsByTagName('tr')
+table2.insertAdjacentElement("beforebegin", c)
 
+// arr a supprimer
+let arr = []
 
+for (let i = 0; i < tr.length; i++) {
+    let th = tr[i].children.namedItem
+    console.log(th)
+    if (i === 1) {
+        for (let j = 0; j < th.length; j++) {
+            arr.push(th[i].textContent)
+        }
+    }
+    // for (let j = 0; j < th.length; j++) {
+    //     console.log(th[j].nodeName)
+    //     if (j === 1) {
+
+    //     }
+    // }
+}
+console.log(arr)
+//Grap for crimes
 let crimes = new Chart(c, {
     // Types of chart we use
     type: 'line',
 
     // The data of the API
     data: {
-        labels: ['2002', '2003', '2004', '2005', '2006', '2007', '2008'],
+        labels: [''],
         datasets: [{
             label: 'Belgium',
-            borderColor: 'rgb(255, 99, 132)',
+            backgroundColor: 'transparent',
             hoverBorderColor: 'rgb(200,80,162)',
-            data: [1012.8, 1007.8, 1013.7, 999.4, 1022.8, 1034.4, 1043.6],
+            data: [],
+
+        }, {
             label: 'Bulgaria',
             borderColor: 'rgb(12, 99, 132)',
+            backgroundColor: 'transparent',
             hoverBorderColor: 'rgb(12,80,162)',
-            data: [146.9, 143.9, 142.1, 137.8, 136.4, 134.7, 126.7]
+            data: [146.9, 143.9, 500, 550, 900, 100, 126.7]
+        }, {
+            label: 'The Netherlands',
+            borderColor: 'rgb(50, 140, 30)',
+            backgroundColor: 'transparent',
+            hoverBorderColor: 'rgb(0,100,20)',
+            data: [1401.9, 1369.3, 1319.5, 1348.3, 1277.8, 1194.0, 1139.7]
         }]
     },
-    options: {}
+    options: {
+    }
 })
+
+console.log(crimes.data.labels)
