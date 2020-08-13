ed7be02296cd56fb4fee9017a3bd6adb9167b1a4
Robin <jacquemart.rob@gmail.com>
2020-08-13 14:51:28 +0200

    API commencement

--- assets/aleks.js
+++ assets/aleks.js
@@ -1,4 +1,33 @@
-//recup des donnés
+let c2 = document.createElement("canvas")
+document.body.appendChild(c2)
+
+
+async function getData() {
+
+    const promise = await fetch('https://canvasjs.com/services/data/datapoints.php%27')
+    const data = await promise.json()
+    console.log(data)
+}
+
+getData();
+
+let canvAPI = new Chart(c2, {
+    // Types of chart we use
+    type: 'bar',
+
+    // The data of the API
+    data: {
+        labels: [],
+        datasets: [],
+    },
+    options: {
+    }
+})
+
+// API 
+// 0    1   - > label = Année 
+//0 1  0 1  -> datasets.data / datasets.labels
+
 let h1 = document.getElementById("firstHeading")
 FETCH();
 
@@ -27,94 +56,3 @@ function FETCH() {
     })();
 }
 
-// let getData = async () => {
-//     let recuperer = await fetch('https://canvasjs.com/services/data/datapoints.php')
-//     let data = await recuperer.json()
-//     return data
-// }
-
-// getData()
-//     .then(data => {
-//         console.log(data)
-//     })
-//     .catch(error => {
-//         alert('ERREUR')
-//         console.log(error)
-//     })
-
-
-let c2 = document.createElement("canvas")
-
-
-h1.insertAdjacentElement("afterend", c2)
-
-// let canvApi = new Chart(c2, {
-//     // Types of chart we use
-//     type: 'line',
-
-//     // The data of the API
-//     data: {
-//         labels: [],
-//         datasets: [{
-//             label: 'Belgium',
-//             backgroundColor: 'transparent',
-//             hoverBorderColor: 'rgb(200,80,162)',
-//             data: [],
-
-//         }]
-//     },
-//     options: {
-//     }
-// })
-
-// canvApi.render()
-
-/////////////////////////////////////////////////////////////
-
-/////Get data from external JSON source and render the chart.
-
-let dataPoints = [];
-$.getJSON("https://canvasjs.com/services/data/datapoints.php", function (data) {
-    $.each(data, function (key, value) {
-        dataPoints.push({ x: value[0], y: parseInt(value[1]) });
-    });
-    chart = new Chart(c2, {
-        title: {
-            text: "Live Chart with dataPoints from External JSON"
-        },
-        data: [{
-            type: "line",
-            dataPoints: dataPoints,
-        }]
-    });
-    chart.render();
-    updateChart();
-});
-
-//////Each time updateChart is called, it gets data from JSON, update 1xsec
-
-function updateChart() {
-    $.getJSON("https://canvasjs.com/services/data/datapoints.php" + (dataPoints.length + 1) + "&ystart=" + (dataPoints[dataPoints.length - 1].y) + "&length=1&type=json", function (data) {
-        $.each(data, function (key, value) {
-            dataPoints.push({
-                x: parseInt(value[0]),
-                y: parseInt(value[1])
-            });
-        });
-        chart.render();
-        setTimeout(function () { updateChart() }, 1000);
-    });
-}
-
-
-////////////////
-
-async function getData() {
-
-    const promise = await fetch('https://canvasjs.com/services/data/datapoints.php%27')
-    const word = await promise.json();
-    console.log(word)
-
-}
-
-getData();
--- assets/canvas.js
+++ assets/canvas.js
@@ -3,12 +3,13 @@
 
 // Canvas Crimes
 let c = document.createElement("canvas")
+
 document.body.appendChild(c)
 
 //Grap for crimes
 let crimes = new Chart(c, {
     // Types of chart we use
-    type: 'line',
+    type: 'bar',
 
     // The data of the API
     data: {
@@ -18,7 +19,6 @@ let crimes = new Chart(c, {
     options: {
     }
 })
-
 
 // Premier Tableau 
 let table1 = document.getElementById("table1")
@@ -33,17 +33,24 @@ for (let i = 2; i < YRows[1].cells.length; i++) {
 }
 
 for (let i = 0; i < YRows.length; i++) {
+    let color = '#' + (Math.random() * 0xFFFFFF << 0).toString(16).padStart(6, '0')
     if (i >= 2) {
         let country = {
             label: '',
-            borderColor: '#' + (Math.random() * 0xFFFFFF << 0).toString(16).padStart(6, '0'),
-            backgroundColor: 'transparent',
+            borderColor: color,
+            backgroundColor: color,
+            //Bar variante 
+            barPercentage: 10,
+            barThickness: 2,
+            maxBarThickness: 15,
+            minBarLength: 40,
             data: []
         }
         country.label = YRows[i].cells[1].innerHTML
         for (let j = 2; j < YRows[i].cells.length; j++) {
             country.data.push(parseInt(YRows[i].cells[j].innerHTML))
         }
+
         crimes.data.datasets.push(country)
     }
 }
