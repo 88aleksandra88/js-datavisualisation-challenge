4e451a35680e66e8d8afcb3330c0f03dd62536b7
slowik <aleksandra.slowik88.gmail.com>
2020-08-13 22:07:51 +0200

    donnés chart ok je pense

--- assets/canvas.js
+++ assets/canvas.js
@@ -54,7 +54,7 @@ for (let i = 0; i < YRows.length; i++) {
     }
 }
 
-///////////////////////////
+///////////////////////////chart 2
 
 let d = document.createElement("canvas")
 document.body.appendChild(d)
@@ -77,32 +77,35 @@ table2.insertAdjacentElement("beforebegin", d)
 let t2Json = []
 let t2Years = []
 let Y2Rows = table2.rows
-for (let i = 2; i < YRows[1].cells.length; i++) {
-    let years = YRows[1].cells[i].innerHTML
+for (let i = 2; i < Y2Rows[1].cells.length; i++) {
+    let years = Y2Rows[1].cells[i].innerHTML
     // t1Years.push(years)  Peut-être à supprimer
     population.data.labels.push(years)
-}
-for (let i = 0; i < YRows.length; i++) {
-    let color = '#' + (Math.random() * 0xFFFFFF << 0).toString(16).padStart(6, '0')
-    if (i >= 2) {
-        let population = {
-            label: '',
-            borderColor: color,
-            backgroundColor: color,
-            //Bar variante 
-            barPercentage: 10,
-            barThickness: 2,
-            maxBarThickness: 15,
-            minBarLength: 40,
-            data: []
+    ///////pas d'erreur jusqu'au la mais 312 en années? whyyyy 
+    for (let i = 0; i < Y2Rows.length; i++) {
+        let color2 = '#' + (Math.random() * 0xFFFFFF << 0).toString(16).padStart(6, '0')
+        if (i >= 2) {
+            let country2 = {
+                label: '',
+                borderColor: 'rgb(255, 99, 132)',
+                backgroundColor: 'rgb(255, 99, 132)',
+                //Bar variante 
+                barPercentage: 10,
+                barThickness: 2,
+                maxBarThickness: 15,
+                minBarLength: 40,
+                data: []
+            }
+            country2.label = Y2Rows[i].cells[1].innerHTML
+            for (let j = 2; j < Y2Rows[i].cells.length; j++) {
+                country2.data.push(parseInt(Y2Rows[i].cells[j].innerHTML))
+            }
+            
+            population.data.datasets.push(country2)
         }
-        population.label = YRows[i].cells[1].innerHTML
-        for (let j = 2; j < YRows[i].cells.length; j++) {
-            country.data.push(parseInt(YRows[i].cells[j].innerHTML))
-        }
-        
-        population.data.datasets.push(country)
+        ///////////////////////yeyyyyyyyyyyy!!!!!!!! ca marcheuhhh mais revoir les couleurs tt ca car c'est moche
     }
+    
+
+
 }
-
-
