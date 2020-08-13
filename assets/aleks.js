b759c96cd543feba155d1628d9dd34d81bc7a579
Robin <jacquemart.rob@gmail.com>
2020-08-13 16:54:56 +0200

    CanvAPI Fonctionne, mais pas Update

--- assets/aleks.js
+++ assets/aleks.js
@@ -1,52 +1,46 @@
 let c2 = document.createElement("canvas")
 document.body.appendChild(c2)
 
-let canvAPI = new Chart(c2, {
-    // Types of chart we use
-    type: 'line',
+let dataPoints = []
+let label = []
 
-    // The data of the API
-    data: {
-        labels: ["API"],
-        datasets: [],
-    },
-    options: {
-    }
-})
-
-// API 
+// // API 
 // 0    1   - > label = Année 
 //0 1  0 1  -> 0 - > x 1 ->
 
 let h1 = document.getElementById("bodyContent")
 h1.insertAdjacentElement("afterbegin", c2)
 
-
 fetch('https://canvasjs.com/services/data/datapoints.php')
     .then(res => res.json())
     .then(data => {
         for (let i = 0; i < data.length; i++) {
-            // canvAPI.data.labels.push(i)
-            let datApi = {
-                label: 'test',
-                borderColor: 'grey',
-                data: []
-            }
-            for (let j = 0; j < data[i].length; j++) {
-                datApi.label = i
-                datApi.data.push(data[i][j])
-            }
-            canvAPI.data.datasets.push(datApi)
-            canvAPI.update()
+            dataPoints.push({ x: data[i][0], y: data[i][1] })
+            label.push(data[i])
         }
+        let canvAPI = new Chart(c2, {
+            // Types of chart we use
+            type: 'line',
+
+            data: {
+                labels: dataPoints,
+                datasets: [{
+                    label: 'Dynamic Graph',
+                    borderColor: 'rgb(255, 99, 132)',
+                    data: dataPoints
+                }]
+            },
+            options: {
+            }
+        })
+        canvAPI.update()
     })
     .catch(err => {
         console.log('error :' + err)
     })
 
-console.log(canvAPI.data.datasets)
 
-//
+
 // FETCH();
 
 // function FETCH() {
@@ -68,3 +62,4 @@ console.log(canvAPI.data.datasets)
 //     })();
 // }
 // setTimeout(function () { updateChart() }, 1000);
+
