8e8687992dfdb467df012895e7458f5b241966fb
Robin <jacquemart.rob@gmail.com>
2020-08-13 12:14:37 +0200

    Premier Graph fonctionnel

--- assets/aleks.js
+++ assets/aleks.js
@@ -9,9 +9,7 @@ function FETCH() {
             if (recuperer.ok) {
                 let data = await recuperer.json().then(data => {
                     // let tableau = data.tableau;
-                    for (let i = 0; i < data.length; i++) {
-                        canvApi.data.labels[i] = data[i]
-                    }
+                    console.log(data)
                     // document.getElementById("firstHeading").innerHTML = tableau
                 });
 
@@ -46,29 +44,77 @@ function FETCH() {
 
 
 let c2 = document.createElement("canvas")
-let ctx2 = c2.getContext('2d')
-c2.width = 400;
-c2.height = 200;
 
-document.body.appendChild(c2)
+
 h1.insertAdjacentElement("afterend", c2)
 
-let canvApi = new Chart(c2, {
-    // Types of chart we use
-    type: 'line',
+// let canvApi = new Chart(c2, {
+//     // Types of chart we use
+//     type: 'line',
 
-    // The data of the API
-    data: {
-        labels: [],
-        datasets: [{
-            label: 'Belgium',
-            backgroundColor: 'transparent',
-            hoverBorderColor: 'rgb(200,80,162)',
-            data: [],
+//     // The data of the API
+//     data: {
+//         labels: [],
+//         datasets: [{
+//             label: 'Belgium',
+//             backgroundColor: 'transparent',
+//             hoverBorderColor: 'rgb(200,80,162)',
+//             data: [],
 
+//         }]
+//     },
+//     options: {
+//     }
+// })
+
+// canvApi.render()
+
+/////////////////////////////////////////////////////////////
+
+/////Get data from external JSON source and render the chart.
+
+let dataPoints = [];
+$.getJSON("https://canvasjs.com/services/data/datapoints.php", function (data) {
+    $.each(data, function (key, value) {
+        dataPoints.push({ x: value[0], y: parseInt(value[1]) });
+    });
+    chart = new Chart(c2, {
+        title: {
+            text: "Live Chart with dataPoints from External JSON"
+        },
+        data: [{
+            type: "line",
+            dataPoints: dataPoints,
         }]
-    },
-    options: {
-    }
-})
+    });
+    chart.render();
+    updateChart();
+});
 
+//////Each time updateChart is called, it gets data from JSON, update 1xsec
+
+function updateChart() {
+    $.getJSON("https://canvasjs.com/services/data/datapoints.php" + (dataPoints.length + 1) + "&ystart=" + (dataPoints[dataPoints.length - 1].y) + "&length=1&type=json", function (data) {
+        $.each(data, function (key, value) {
+            dataPoints.push({
+                x: parseInt(value[0]),
+                y: parseInt(value[1])
+            });
+        });
+        chart.render();
+        setTimeout(function () { updateChart() }, 1000);
+    });
+}
+
+
+////////////////
+
+async function getData() {
+
+    const promise = await fetch('https://canvasjs.com/services/data/datapoints.php%27')
+    const word = await promise.json();
+    console.log(word)
+
+}
+
+getData();
--- assets/canvas.js
+++ assets/canvas.js
@@ -1,33 +1,9 @@
 //Creer un canvas 
 // Insérer le canvas au dessus des tableaux de données
-// Premier Tableau 
-let table1 = document.getElementById("table1")
-let table2 = document.getElementById("table2")
 
 // Canvas Crimes
 let c = document.createElement("canvas")
-let ctx = c.getContext('2d')
-c.width = 400;
-c.height = 200;
-
 document.body.appendChild(c)
-table1.insertAdjacentElement("beforebegin", c)
-let tr = table1.getElementsByTagName('tr')
-table2.insertAdjacentElement("beforebegin", c)
-
-
-let rowLength = table1.rows.length
-// table2.insertAdjacentElement("beforebegin", c)
-
-// for (let i = 0; i < rowLength; i++) {
-//     let cell = table1.rows.item(i).cells;
-//     let cellLength = cell.length;
-
-//     for (let j = 0; j < cellLength; j++) {
-//         console.log(cell.item(j).innerHTML)
-//     }
-// }
-
 
 //Grap for crimes
 let crimes = new Chart(c, {
@@ -36,27 +12,39 @@ let crimes = new Chart(c, {
 
     // The data of the API
     data: {
-        labels: [''],
-        datasets: [{
-            label: 'Belgium',
-            backgroundColor: 'transparent',
-            hoverBorderColor: 'rgb(200,80,162)',
-            data: [],
-
-        }, {
-            label: 'Bulgaria',
-            borderColor: 'rgb(12, 99, 132)',
-            backgroundColor: 'transparent',
-            hoverBorderColor: 'rgb(12,80,162)',
-            data: [146.9, 143.9, 500, 550, 900, 100, 126.7]
-        }, {
-            label: 'The Netherlands',
-            borderColor: 'rgb(50, 140, 30)',
-            backgroundColor: 'transparent',
-            hoverBorderColor: 'rgb(0,100,20)',
-            data: [1401.9, 1369.3, 1319.5, 1348.3, 1277.8, 1194.0, 1139.7]
-        }]
+        labels: [],
+        datasets: []
     },
     options: {
     }
 })
+
+
+// Premier Tableau 
+let table1 = document.getElementById("table1")
+table1.insertAdjacentElement("beforebegin", c)
+let t1Json = []
+let t1Years = []
+let YRows = table1.rows
+for (let i = 2; i < YRows[1].cells.length; i++) {
+    let years = YRows[1].cells[i].innerHTML
+    // t1Years.push(years)  Peut-être à supprimer
+    crimes.data.labels.push(years)
+}
+
+for (let i = 0; i < YRows.length; i++) {
+    if (i >= 2) {
+        let country = {
+            label: '',
+            borderColor: '#' + (Math.random() * 0xFFFFFF << 0).toString(16).padStart(6, '0'),
+            backgroundColor: 'transparent',
+            data: []
+        }
+        country.label = YRows[i].cells[1].innerHTML
+        for (let j = 2; j < YRows[i].cells.length; j++) {
+            country.data.push(parseInt(YRows[i].cells[j].innerHTML))
+        }
+        crimes.data.datasets.push(country)
+    }
+}
+
